6ca891e4819e92e8ad9dbb1a4a2b1448edc9425f
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-04-18 20:32:30 +0100

    Adição do projeto Mini Game

---
+++ MiniGame/Weapon.cs
@@ -0,0 +1,12 @@
+namespace MiniGame
+{
+    public class Weapon
+    {
+        protected float power;
+
+        public Weapon(float power)
+        {
+            this.power = power;
+        }
+    }
+}
